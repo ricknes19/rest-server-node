9a462bcdb4efde57f8750bc734f104503e235494
ricknes19 <ricknes.dev23@gmail.com>
2021-05-08 18:24:54 -0400

    Implementando el servicio de obtener usuarios

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -3,10 +3,20 @@ const bcryptjs = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
 
-const usuariosGet = (req = request, res = response) => {
+const usuariosGet = async(req = request, res = response) => {
+
+    const { limite = 5, desde = 0} = req.query;
+    const query = { estado: true };
+
+    const [ total, usuarios ] = await Promise.all([
+        Usuario.countDocuments(query),
+        Usuario.find(query).skip(Number(desde)).limit(Number(limite))
+    ]);
+
 
     res.json({
-        msg: 'GET usuario'
+        total,
+        usuarios
     });
 
 }
